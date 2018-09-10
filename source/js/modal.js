6459c78c52e54766666a834badb7c3e7e0abedd1
iashchuk <221box@list.ru>
2018-09-11 02:58:40 +0300

    Дорабатывает функцию закрытия моодального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -4,31 +4,37 @@
 
   var buttonOrder = document.querySelector(".hit__button");
   var order = document.querySelector(".order");
-  var page = document.querySelector(".order__overlay");
+  var page = document.querySelector(".page");
+  var overlay = document.querySelector(".order__overlay");
+
+
+  var closeModal = function(evt) {
+    console.log(evt);
+    if (evt.target === overlay) {
+      if (order.classList.contains("order--show")) {
+        order.classList.remove("order--show");
+        page.removeEventListener("click", closeModal);
+        page.removeEventListener("touchstart", closeModal);
+      }
+    }
+  }
 
   buttonOrder.addEventListener("click", function (evt) {
+    var target = evt.target;
+    var block = target.parentNode;
     evt.preventDefault();
     order.classList.add("order--show");
+
+    page.addEventListener("click", closeModal);
+    page.addEventListener("touchstart", closeModal);
   });
 
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (order.classList.contains("order--show")) {
-        order.classList.remove("order--show");
-      }
+      closeModal();
     }
   });
-
-
-  document.addEventListener("click", function (evt) {
-    if (evt.target === page) {
-      if (order.classList.contains("order--show")) {
-        order.classList.remove("order--show");
-      }
-    }
-  });
-
 
 })();
